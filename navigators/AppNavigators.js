17da937e0333594c6836a4f57a7eab65b17db44a
Z-LianYan <1371935318@qq.com>
2019-02-17 21:05:24 +0800

    解决冲突

--- navigators/AppNavigators.js
+++ navigators/AppNavigators.js
@@ -170,3 +170,21 @@ export default App = createAppContainer(AppStackNavigator)
 
 
 // cd android && gradlew assembleRelease
+
+
+// git filter-branch --env-filter '
+// OLD_EMAIL="2930638161@qq.com"
+// CORRECT_NAME="Z-LianYan"
+// CORRECT_EMAIL="1371935318@qq.com"
+// if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+// then
+//     export GIT_COMMITTER_NAME="$CORRECT_NAME"
+//     export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+// fi
+// if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+// then
+//     export GIT_AUTHOR_NAME="$CORRECT_NAME"
+//     export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+// fi
+// ' --tag-name-filter cat -- --branches --tags
+
